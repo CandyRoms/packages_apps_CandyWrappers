cd8e31ac1bb06a3741bac6147b7031329b0b8e6a
Dan Cartier <nospamdan1@gmail.com>
2018-09-11 19:40:31 -0400

    res: Reorganize and clean up
    * Drop the ".png" and .jpg" from the wallpapers array
    * New app icon (red candy on black)
    * Simple is good :)

--- Android.mk
+++ Android.mk
@@ -9,14 +9,23 @@
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES += $(call all-java-files-under, src)
-
 LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_SRC_FILES += $(call all-java-files-under, src)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4
+LOCAL_AAPT_FLAGS := --auto-add-overlay
 
-LOCAL_PACKAGE_NAME := XtendedWallz
-
+LOCAL_SDK_VERSION := current
+LOCAL_PACKAGE_NAME := CandyWrappers
 include $(BUILD_PACKAGE)
+
+# Static library for target
+# ========================================================
+include $(CLEAR_VARS)
+LOCAL_MODULE := libCandyWrappers
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_SDK_VERSION := current
+LOCAL_SRC_FILES := $(call all-java-files-under,src)
+LOCAL_JAVA_LIBRARIES := android-support-v4
+include $(BUILD_STATIC_JAVA_LIBRARY)
